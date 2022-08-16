2d9cdee3e0bd67eb3f80f136bef5d3ed117a9fe4
Lucas Martin-King <lmartinking@gmail.com>
2022-08-16 22:44:43 +1000

    Add `bin/seed_tx.rs` for sending funds from the seed account

---
+++ src/bin/seed_tx.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::str::FromStr;
+
+use capycoin::message::ServerMessage;
+use uuid::Uuid;
+
+use capycoin::{client_util, message::ClientMessage};
+use capycoin::account::seed_account_id;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Expected two arguments: RECEIVER_ACCOUNT_ID AMOUNT");
+        return;
+    }
+
+    let rx_account = Uuid::from_str(&args[1].as_str()).unwrap();
+    let amount: i64 = args[2].parse().unwrap();
+
+    let seed_account_id = seed_account_id();
+    let msg = ClientMessage::CreateTransaction { sender_id: seed_account_id, receiver_id: rx_account, amount: amount };
+
+    let resp = client_util::send_and_recv_msg(msg).expect("Expected a response from core!");
+
+    match resp {
+        ServerMessage::CreateTransactionResponse(Ok(r)) => {
+            println!("Transaction receipt: {:?}", r)
+        },
+        ServerMessage::CreateTransactionResponse(Err(err)) => {
+            eprintln!("Transaction error: {:?}", err)
+        },
+        _ => panic!("Unexpected response from server!")
+    }
+}
